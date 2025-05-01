75e48f44ef6b7c701133d492806c4201f6fc1b58
fatahburiro66 <f18086863@gmail.com>
2025-05-01 12:50:32 +0500

    Protected Routes Done Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Navbar from './components/Navbar'
+import PublicRoute from './components/PublicRoute'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
+
+  const [isAuthenticated, setAuth] = useState(!!localStorage.getItem('token'))
+
   return (
-    <div>App</div>
+    <div>
+      <Navbar isAuthenticated={isAuthenticated} setAuth={setAuth} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={
+          <PublicRoute>
+            < Login />
+          </PublicRoute>
+        } />
+
+        <Route path="/register" element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        } />
+
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App 
